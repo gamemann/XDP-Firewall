f0a1d6e98d64b41387752c974e306a711b27d4b1
gamemann <gamemann@gflclan.com>
2021-11-12 17:31:50 +0000

    Reorder cmdline structure for organization.

--- src/cmdline.h
+++ src/cmdline.h
@@ -3,10 +3,10 @@
 struct cmdline
 {
     char *cfgfile;
-    unsigned int help : 1;
-    unsigned int list : 1;
     unsigned int offload : 1;
     unsigned int skb : 1;
+    unsigned int list : 1;
+    unsigned int help : 1;
 };
 
 void parsecommandline(struct cmdline *cmd, int argc, char *argv[]);
