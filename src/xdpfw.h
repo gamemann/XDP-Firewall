177c48adcc0adbf1d6deee086fe68b02a2b11ea2
gamemann <gamemann@gflclan.com>
2021-11-12 17:34:13 +0000

    Move common BPF includes to header file.

--- src/xdpfw.h
+++ src/xdpfw.h
@@ -112,3 +112,21 @@ struct ip_stats
     __u64 bps;
     __u64 tracking;
 };
+
+#ifdef __BPF__
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define htons(x) ((__be16)___constant_swab16((x)))
+#define ntohs(x) ((__be16)___constant_swab16((x)))
+#define htonl(x) ((__be32)___constant_swab32((x)))
+#define ntohl(x) ((__be32)___constant_swab32((x)))
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define htons(x) (x)
+#define ntohs(X) (x)
+#define htonl(x) (x)
+#define ntohl(x) (x)
+#endif
+
+#define uint128_t __uint128_t
+#endif
