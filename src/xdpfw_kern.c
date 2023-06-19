d5fe99c69a4ce81ba2bf3c45c3559a8842638879
Christian Deacon <christian@moddingcommunity.com>
2023-06-19 19:12:38 +0000

    XDP tools already defines bpf_printk() now.

--- src/xdpfw_kern.c
+++ src/xdpfw_kern.c
@@ -17,14 +17,6 @@
 
 #include "xdpfw.h"
 
-#ifdef DEBUG
-#define bpf_printk(fmt, ...) \
-({ \
-    char ____fmt[] = fmt; \
-    bpf_trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); \
-})
-#endif
-
 #ifndef memcpy
 #define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
 #endif
