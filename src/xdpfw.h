62c610f577c951796c85a9e8e945852672a71c74
Christian Deacon <christianmdeacon@gmail.com>
2024-07-15 21:44:13 -0400

    Decrease max filters from 80 to 60.

--- src/xdpfw.h
+++ src/xdpfw.h
@@ -3,7 +3,7 @@
 #include <linux/types.h>
 
 #define MAX_PCKT_LENGTH 65535
-#define MAX_FILTERS 80
+#define MAX_FILTERS 60
 #define MAX_TRACK_IPS 100000
 #define MAX_CPUS 256
 #define NANO_TO_SEC 1000000000
