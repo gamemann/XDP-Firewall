b6b43b67c28466a1cb652c3306756d43e3ccfff7
Christian Deacon <christianmdeacon@gmail.com>
2025-02-23 06:33:40 -0500

    Add Loader stats source files.

---
+++ src/loader/utils/stats.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <bpf.h>
+#include <libbpf.h>
+#include <xdp/libxdp.h>
+
+#include  <common/all.h>
+
+#include <loader/utils/cmdline.h>
+#include <loader/utils/config.h>
+#include <loader/utils/helpers.h>
+
+int CalculateStats(int stats_map, int cpus);
