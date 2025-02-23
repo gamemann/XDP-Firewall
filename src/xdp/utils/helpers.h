6047718c6aa996c68681fbefd445a4a98aefe2a3
Christian Deacon <christianmdeacon@gmail.com>
2025-02-23 07:43:12 -0500

    Add another note.

--- src/xdp/utils/helpers.h
+++ src/xdp/utils/helpers.h
@@ -31,5 +31,6 @@
 static __always_inline int IsIpInRange(u32 src_ip, u32 net_ip, u8 cidr);
 
 // The source file is included directly below instead of compiled and linked as an object because when linking, there is no guarantee the compiler will inline the function (which is crucial for performance).
+// I'd prefer not to include the function logic inside of the header file.
 // More Info: https://stackoverflow.com/questions/24289599/always-inline-does-not-work-when-function-is-implemented-in-different-file
 #include "helpers.c"
--- src/xdp/utils/rl.h
+++ src/xdp/utils/rl.h
@@ -11,5 +11,6 @@ static __always_inline void UpdateIp6Stats(u64 *pps, u64 *bps, u128 *ip, u16 por
 
 
 // The source file is included directly below instead of compiled and linked as an object because when linking, there is no guarantee the compiler will inline the function (which is crucial for performance).
+// I'd prefer not to include the function logic inside of the header file.
 // More Info: https://stackoverflow.com/questions/24289599/always-inline-does-not-work-when-function-is-implemented-in-different-file
 #include "rl.c"
