ff1ac4e8175031d4c0933eb56e280e44657dfb0b
Christian Deacon <christianmdeacon@gmail.com>
2025-03-01 10:38:45 -0500

    Add support for IP range drop and make features more modular.

--- src/xdp/utils/helpers.c
+++ src/xdp/utils/helpers.c
@@ -1,5 +1,7 @@
 #include <xdp/utils/helpers.h>
 
+#include <xdp/utils/maps.h>
+
 /**
  * Checks if an IP is within a specific CIDR range.
  * 
@@ -13,3 +15,37 @@ static __always_inline int IsIpInRange(u32 src_ip, u32 net_ip, u8 cidr)
 {
     return !((src_ip ^ net_ip) & htonl(0xFFFFFFFFu << (32 - cidr)));
 }
+
+#ifdef ENABLE_IP_RANGE_DROP
+/**
+ * Checks if the IP is in the IP range drop map.
+ * 
+ * @param ip The IP address.
+ * 
+ * @return 1 on yes or 0 on no.
+ */
+static __always_inline int CheckIpRangeDrop(u32 ip)
+{
+    LpmTrieKey key =
+    {
+        .prefix_len = 32,
+        .data = ip
+    };
+
+    u64 *lookup = bpf_map_lookup_elem(&map_range_drop, &key);
+
+    if (lookup)
+    {
+        u32 bit_mask = *lookup >> 32;
+        u32 prefix = *lookup & 0xFFFFFFFF;
+
+        // Check if matched.
+        if ((ip & bit_mask) == prefix)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+#endif
