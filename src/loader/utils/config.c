d35ebf54ceab671b5e05def7fffb9849ab64dd1d
Christian Deacon <christianmdeacon@gmail.com>
2025-03-28 12:06:51 -0400

    Fix CLI override for interface not working if interface option doesn't exist inside of config.

--- src/loader/utils/config.c
+++ src/loader/utils/config.c
@@ -273,6 +273,17 @@ int parse_cfg(config__t *cfg, const char* data, config_overrides_t* overrides)
             }
         }
     }
+    else if (overrides && overrides->interface)
+    {
+        if (cfg->interfaces[0])
+        {
+            free(cfg->interfaces[0]);
+            cfg->interfaces[0] = NULL;
+        }
+
+        cfg->interfaces[0] = strdup(overrides->interface);
+        cfg->interfaces_cnt = 1;
+    }
 
     // Pin BPF maps.
     int pin_maps;
@@ -1433,7 +1444,7 @@ void print_filter(filter_rule_cfg_t* filter, int idx)
 
     // ICMP Options.
     printf("\t\tICMP Options\n");
-    
+
     printf("\t\t\tICMP Enabled => %d\n", filter->icmp.enabled);
     printf("\t\t\tICMP Code => %d\n", filter->icmp.code);
     printf("\t\t\tICMP Type => %d\n", filter->icmp.type);
